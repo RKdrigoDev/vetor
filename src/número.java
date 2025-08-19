9438eb5b21171298a9e72d517fb804cd80d434e5
RkdrigoDev <labsfiap@fiap.com.br>
2025-08-19 20:45:28 -0300

    commit

--- src/número.java
+++ src/número.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class número {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int valor;
+        int[] number= new int[10];
+        for (int i=0; i<number.length; i++){
+            System.out.println("digite um número");
+            number[i]=sc.nextInt();
+            if (number[i]<0){
+                System.out.println("não pode");
+            }
+
+        }
+        System.out.println("\nDados antes da inverção");
+        for (int i=0; i<number.length; i++){
+            System.out.println(number[i]+" ");
+        }
+        System.out.println("\nDados antes depois da inversão");
+        for (int i=0; i<number.length; i++){
+            System.out.println(number[i]+" ");
+        }
+    }
 }
