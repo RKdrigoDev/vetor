eb974b681ab06c1440f3302489f02490ae4c31f5
RkdrigoDev <labsfiap@fiap.com.br>
2025-08-19 20:55:57 -0300

    commit

--- src/número.java
+++ src/número.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class número {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int valor;
+        int valor, aux;
         int[] number= new int[10];
         for (int i=0; i<number.length; i++){
             System.out.println("digite um número");
@@ -17,7 +17,14 @@ public class número {
         for (int i=0; i<number.length; i++){
             System.out.println(number[i]+" ");
         }
-        System.out.println("\nDados antes depois da inversão");
+
+        for (int i=0; i<number.length/2; i++){
+            aux=number[i];
+            number[i]= number[number.length-1];
+            number[number.length-1]=aux;
+            System.out.println(number[i]+" ");
+        }
+        System.out.println("\nDados  depois da inversão");
         for (int i=0; i<number.length; i++){
             System.out.println(number[i]+" ");
         }
