8224f7e537a4ea226138f3358098199660b49841
RkdrigoDev <labsfiap@fiap.com.br>
2025-08-19 22:06:25 -0300

    commit

--- src/número.java
+++ src/número.java
@@ -8,10 +8,6 @@ public class número {
         for (int i=0; i<number.length; i++){
             System.out.println("digite um número");
             number[i]=sc.nextInt();
-            if (number[i]<0){
-                System.out.println("não pode");
-            }
-
         }
         System.out.println("\nDados antes da inverção");
         for (int i=0; i<number.length; i++){
@@ -20,9 +16,9 @@ public class número {
 
         for (int i=0; i<number.length/2; i++){
             aux=number[i];
-            number[i]= number[number.length-1];
-            number[number.length-1]=aux;
-            System.out.println(number[i]+" ");
+            number[i]= number[number.length-1-i];
+            number[number.length-1-i]=aux;
+
         }
         System.out.println("\nDados  depois da inversão");
         for (int i=0; i<number.length; i++){
